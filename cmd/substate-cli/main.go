e727284b3e584724a0fb9c49eaa1121d1e910252
wsodsong <wasuwee.sodsong@gmail.com>
2022-08-31 12:14:11 +0200

    add a script printing storage in flat data format

--- cmd/substate-cli/main.go
+++ cmd/substate-cli/main.go
@@ -35,6 +35,7 @@ func init() {
 	app.Commands = []cli.Command{
 		replay.ReplayCommand,
 		replay.GetStorageUpdateSizeCommand,
+		replay.GetStorageFlatDataCommand,
 		replay.GetCodeCommand,
 		replay.GetCodeSizeCommand,
 		replay.SubstateDumpCommand,
